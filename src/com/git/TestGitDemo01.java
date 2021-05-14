1f9776a4c3238a5450d00f1b24a42acfb4c00b23
luorong <1687075602@qq.com>
2021-05-14 21:49:22 +0800

    再次修改了testgitdemo1

--- src/com/git/TestGitDemo01.java
+++ src/com/git/TestGitDemo01.java
@@ -4,5 +4,12 @@ public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("这是git01");
         System.out.println("我修改了TestDemo01");
+
+    }
+
+    public void add() {
+        System.out.println("增加方法1");
     }
 }
+
+
