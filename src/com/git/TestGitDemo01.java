3188b306372dbc11af41d66ea1723a6cbc3ad0b9
luorong <1687075602@qq.com>
2021-05-14 20:57:23 +0800

    我修改了TestDemo01

--- src/com/git/TestGitDemo01.java
+++ src/com/git/TestGitDemo01.java
@@ -3,5 +3,6 @@ package com.git;
 public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("这是git01");
+        System.out.println("我修改了TestDemo01");
     }
 }
